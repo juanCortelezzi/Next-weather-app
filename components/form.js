0046b5861450a139e24ba5826804224945bd7e1e
juancortelezzi <juanbicortelezzi@gmail.com>
2020-08-07 01:37:57 -0300

    Form.js from rcc to rfc

--- components/form.js
+++ components/form.js
@@ -1,40 +1,39 @@
 import validate from "./validate.js";
 import Router from "next/router";
+import { useState } from "react";
 
-export default class Form extends React.Component {
-  state = {};
+export default function Form() {
+  const [q, setq] = useState("");
 
-  handleChange = ({ target }) => {
+  const handleChange = ({ target }) => {
     const { value } = target;
-    this.setState({ q: value });
+    setq(value);
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const valid = validate(this.state.q);
+    const valid = validate(q);
     if (valid.valid) {
       Router.push(`/?q=${valid.query}`);
       e.target.reset();
     } else {
-      alert(`${this.state.q} is not valid`);
+      alert(`${q} is not valid`);
     }
   };
 
-  render() {
-    return (
-      <form autoComplete="off" onSubmit={this.handleSubmit}>
-        <label htmlFor="q">
-          <img id="location-logo" src="/location-logo.svg" alt="location logo" />
-        </label>
-        <input
-          type="text"
-          name="q"
-          id="q"
-          placeholder="ej.New York,US or Buenos Aires,AR"
-          onChange={this.handleChange}
-          required
-        />
-      </form>
-    );
-  }
+  return (
+    <form autoComplete="off" onSubmit={handleSubmit}>
+      <label htmlFor="q">
+        <img id="location-logo" src="/location-logo.svg" alt="location logo" />
+      </label>
+      <input
+        type="text"
+        name="q"
+        id="q"
+        placeholder="ej.New York,US or Buenos Aires,AR"
+        onChange={handleChange}
+        required
+      />
+    </form>
+  );
 }
