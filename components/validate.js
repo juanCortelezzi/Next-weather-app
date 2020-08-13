f381854b49b521c402bea75bd4e778f1da9d5d33
juancortelezzi <juanbicortelezzi@gmail.com>
2020-08-13 02:04:09 -0300

    made the ",(two letter country code)" optional in both GUI and CLI

--- components/validate.js
+++ components/validate.js
@@ -1,29 +1,49 @@
 export default function validate(query, bash = false) {
+  // asked for validation from browser
   if (!bash) {
-    const mainRegex = /^[a-zA-Z ]{1,30},[a-zA-Z ]{2,10}$/;
+    const mainRegex = /^[a-zA-Z ]{1,40}(,[a-zA-Z ]{2,10})?$/;
     const testMainRegex = mainRegex.test(query);
 
     if (!testMainRegex) return { query: query, valid: testMainRegex };
 
+    query = query.replace(/^\s+|\s(?=\s)|\s+$/g, "");
+
+    const hasCommaRegex = /.*,.*/;
+    const testHasCommaRegex = hasCommaRegex.test(query);
+
+    if (testHasCommaRegex) {
+      const splitQuery = query.split(",");
+      const city = splitQuery[0].replace(/\s+$/g, "");
+      const country = splitQuery[1].replace(/\s+/g, "");
+      const finalQuery = `${city},${country}`;
+      return { query: finalQuery, valid: testMainRegex };
+    }
+
+    return { query: query, valid: testMainRegex };
+  }
+
+  // asked for validation from terminal (curl)
+
+  const mainRegex = /^[a-zA-Z-]{1,40}(,[a-zA-Z]{2})?$/;
+  const testMainRegex = mainRegex.test(query);
+
+  if (!testMainRegex) return { query: query, valid: testMainRegex };
+
+  query = query.replace(/^-+|-(?=-)|-+$/g, "");
+
+  const hasCommaRegex = /^.*,.*$/;
+  const testHasCommaRegex = hasCommaRegex.test(query);
+
+  if (testHasCommaRegex) {
     const splitQuery = query.split(",");
-    const city = splitQuery[0].replace(/^\s+|\s(?=\s)|\s+$/g, "");
-    const country = splitQuery[1].replace(/\s+/g, "");
-
-    const finalQuery = `${city},${country}`;
-
-    return { query: finalQuery, valid: testMainRegex };
-  } else {
-    const mainRegex = /^[a-zA-Z-]{1,30},[a-zA-Z]{2}$/;
-    const testMainRegex = mainRegex.test(query);
-
-    if (!testMainRegex) return { query: query, valid: testMainRegex };
-
-    const splitQuery = query.split(",");
-    const city1 = splitQuery[0].replace(/^-+|-(?=-)|-+$/g, "");
-    const city2 = city1.replace(/-/g, " ");
-    const country = splitQuery[1].replace(/-+/g, "");
-    const finalQuery = `${city2},${country}`;
-
+    let city1 = splitQuery[0].replace(/-+$/g, "");
+    city1 = city1.replace(/-/g, " ");
+    const country = splitQuery[1];
+    const finalQuery = `${city1},${country}`;
     return { query: finalQuery, valid: testMainRegex };
   }
+
+  let finalQuery = query.replace(/-+$/g, "");
+  finalQuery = finalQuery.replace(/-/g, " ");
+  return { query: finalQuery, valid: testMainRegex };
 }
